b843a014a71bf9f86b28ad950426fc0517523860
Graham Kilmon <ggkilmon@gmail.com>
2021-03-09 21:29:56 -0500

    refactor banner into component

--- angular-component-refactor/src/app/app.module.ts
+++ angular-component-refactor/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChairImageComponent } from './chair-image/chair-image.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChairImageComponent,
+    BannerComponent,
   ],
   imports: [
     BrowserModule,
