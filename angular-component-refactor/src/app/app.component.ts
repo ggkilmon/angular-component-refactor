00f310af847bede3d5e48c8c90c94fa98ff76b5f
Graham Kilmon <ggkilmon@gmail.com>
2021-03-09 21:39:15 -0500

    create chair object and use array to display

--- angular-component-refactor/src/app/app.component.ts
+++ angular-component-refactor/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Chair } from './models/chair.model';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-component-refactor';
   selectedChair: string = "";
+
+  chairs: Chair[] = [];
+
+  ngOnInit(){
+    this.chairs = [
+      {name: "Aeron", imageUrl: "/assets/aeron.jpg"},
+      {name: "Cosm", imageUrl: "/assets/cosm.jpg"},
+      {name: "Embody", imageUrl: "/assets/embody.jpg"},
+      {name: "Mirra 2", imageUrl: "/assets/mirra2.jpg"},
+      {name: "Celle", imageUrl: "/assets/celle.jpg"},
+    ];
+  }
 
   selectChair(chair: string){
     console.log(chair);
---
+++ angular-component-refactor/src/app/models/chair.model.ts
@@ -0,0 +1,4 @@
+export interface Chair {
+    imageUrl: string;
+    name: string;
+}
